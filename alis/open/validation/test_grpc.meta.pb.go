d190f779cc8720f537b1edeef270fd7c3a57c55c
alis-build <admin@alis.build>
2026-08-06 08:43:45 +0000

    Protoc generated code for version 1.4.0

--- alis/open/validation/test_grpc.meta.pb.go
+++ alis/open/validation/test_grpc.meta.pb.go
@@ -3,7 +3,7 @@
 // Source: alis/open/validation/v1/test.proto
 // Plugin version: v0.0.1
 //
-// Generated on: 2026-08-03 06:05:50 UTC
+// Generated on: 2026-08-06 08:43:44 UTC
 
 package validation
 
