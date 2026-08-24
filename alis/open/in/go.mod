42878421b22e46940ad4b605911f6b00a2d43047
alis-build <admin@alis.build>
2026-08-24 16:24:06 +0000

    Protoc generated code for version 1.4.0

--- alis/open/in/go.mod
+++ alis/open/in/go.mod
@@ -1,4 +1,4 @@
-module github.com/alis-build/public-go/alis/open/in
+module go.alis.build/common/alis/open/in
 go 1.26
 
 require ()
--- alis/open/in/in_grpc.meta.pb.go
+++ alis/open/in/in_grpc.meta.pb.go
@@ -3,7 +3,7 @@
 // Source: alis/open/in/v1/in.proto
 // Plugin version: v0.0.1
 //
-// Generated on: 2026-07-02 09:31:40 UTC
+// Generated on: 2026-08-24 16:24:05 UTC
 
 package in
 
