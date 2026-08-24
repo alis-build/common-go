c8744fac03f33da17588b8af875544e75144789a
alis-build <admin@alis.build>
2026-08-24 16:20:50 +0000

    Protoc generated code for version 1.6.0

--- alis/a2a/extension/history/history_grpc.meta.pb.go
+++ alis/a2a/extension/history/history_grpc.meta.pb.go
@@ -3,7 +3,7 @@
 // Source: alis/a2a/extension/history/v1/history.proto
 // Plugin version: v0.0.1
 //
-// Generated on: 2026-08-06 08:42:51 UTC
+// Generated on: 2026-08-24 16:20:49 UTC
 
 package history
 
