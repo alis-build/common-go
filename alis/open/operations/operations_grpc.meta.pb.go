103617664f7adac8ab4ac87ee17d59abd4135466
alis-build <admin@alis.build>
2026-09-01 13:22:31 +0000

    Protoc generated code for version 1.8.0

--- alis/open/operations/operations_grpc.meta.pb.go
+++ alis/open/operations/operations_grpc.meta.pb.go
@@ -3,7 +3,7 @@
 // Source: alis/open/operations/v1/operations.proto
 // Plugin version: v0.0.1
 //
-// Generated on: 2026-09-01 10:28:08 UTC
+// Generated on: 2026-09-01 13:22:30 UTC
 
 package operations
 
