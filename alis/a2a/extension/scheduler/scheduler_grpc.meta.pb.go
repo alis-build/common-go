f619b7f927afbf8a5ebed7f7d5f6643a90c44114
alis-build <admin@alis.build>
2026-09-01 10:25:52 +0000

    Protoc generated code for version 1.8.0

--- alis/a2a/extension/scheduler/scheduler_grpc.meta.pb.go
+++ alis/a2a/extension/scheduler/scheduler_grpc.meta.pb.go
@@ -3,7 +3,7 @@
 // Source: alis/a2a/extension/scheduler/v1/scheduler.proto
 // Plugin version: v0.0.1
 //
-// Generated on: 2026-08-24 16:21:14 UTC
+// Generated on: 2026-09-01 10:25:51 UTC
 
 package scheduler
 
