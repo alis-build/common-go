ffb542d35fb58e3985c0ea567dee69bed5ee16e8
alis-build <admin@alis.build>
2026-09-02 16:27:47 +0000

    Protoc generated code for version 1.23.0

--- alis/adk/sessions/sessions_grpc.meta.pb.go
+++ alis/adk/sessions/sessions_grpc.meta.pb.go
@@ -3,7 +3,7 @@
 // Source: alis/adk/sessions/v1/sessions.proto
 // Plugin version: v0.0.1
 //
-// Generated on: 2026-09-02 14:29:00 UTC
+// Generated on: 2026-09-02 16:27:45 UTC
 
 package sessions
 
